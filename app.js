c28a81e69bb557bee7079baf36022021903bbea2
gracewiseman <lc22-0338@lclark.edu>
2026-04-08 13:18:50 -0700

    edit2

--- app.js
+++ app.js
@@ -6,10 +6,12 @@
 // console.log("end");
 
 const http = require("http"); 
-const server = http.createServer((req,res) =>{
-    res.write("hello hellor");
-    res.end(); 
 
+const server = http.createServer((req,res) =>{
+    fstat.readFile("index.html", (err,data) =>{
+        res.writeHead(200,{"Contemnt-Type":"text/html"});
+    res.write(data);
+    res.end(); 
 });
 server.listen(3000, () => {
     console.log("server running on");
