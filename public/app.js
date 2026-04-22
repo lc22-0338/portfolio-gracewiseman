2d2102d57be0f0aeadc9941bde5a5adf9f197d88
gracewiseman <lc22-0338@lclark.edu>
2026-04-22 11:44:28 -0700

    project and about page exists

--- public/app.js
+++ public/app.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const path = require("path")
 
+const mysql = require("mysql")
+const connection = mysql.createConnection({
+    host : "localhost",
+    user: "root",
+    password:"PioS&&!*7718",
+})
 const app = express();
 const PORT = 3000;
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(__dirname))
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`)
 })
